e57a5a027e878c32b7798d1ca907bea532015364
William Suratt <williamsuratt@ip0af576fe.int.uvm.edu>
2021-03-11 23:17:01 -0500

    commit all changes

--- client/core/Menu.js
+++ client/core/Menu.js
@@ -4,11 +4,11 @@ import {isMobile} from 'react-device-detect';
 import DesktopMenu from './DesktopMenu'
 import MobileMenu from './MobileMenu'
 
-const isMobile = isMobile
+const isApp = isMobile
 
 const Menu = () => (
   <div>
-    if (isMobile) {
+    if (isApp) {
       <MobileMenu/>
     }
     else {
