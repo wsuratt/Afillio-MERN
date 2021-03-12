fd2db84e776a6680bc4013edb6e27b5fa9890bfe
William Suratt <williamsuratt@ip0af576fe.int.uvm.edu>
2021-03-12 00:17:41 -0500

    commit all changes

--- client/core/Menu.js
+++ client/core/Menu.js
@@ -1,20 +1,19 @@
 import React from 'react'
+import {isMobile} from 'react-device-detect';
 import {BrowserView, MobileView} from "react-device-detect";
 import DesktopMenu from './DesktopMenu'
 import MobileMenu from './MobileMenu'
 
 const Menu = () => {
+  if(isMobile) {
+    return (
+        <div><DesktopMenu/></div>
+    )
+  }
   return (
-    <>
-        <BrowserView>
-          <DesktopMenu/>
-        </BrowserView>
-        <MobileView>
-          <MobileMenu/>
-        </MobileView>
-    </>
+      <div><MobileMenu/></div>
   );
   
-  }
+}
 
 export default Menu
