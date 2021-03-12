fba069599f982d3f4ada007fa70ad41ff920cc4e
William Suratt <williamsuratt@ip0af576fe.int.uvm.edu>
2021-03-11 21:40:49 -0500

    commit all changes

--- client/core/MobileMenu.js
+++ client/core/MobileMenu.js
@@ -14,6 +14,7 @@ import { useHistory } from "react-router-dom";
 
 const useStyles = makeStyles(theme => ({
     title: {
+        display: "block",
         flexGrow: 1,
         textAlign: "center",
         fontSize: "x-large"
