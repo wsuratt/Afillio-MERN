4bf857942060580e54938ec64a46f7366fc0d92a
William Suratt <williamsuratt@ip0af576fe.int.uvm.edu>
2021-03-13 00:30:21 -0500

    commit all changes

--- client/core/Menu.js
+++ client/core/Menu.js
@@ -4,14 +4,14 @@ import DesktopMenu from './DesktopMenu'
 import MobileMenu from './MobileMenu'
 
 const Menu = () => (
-  <div>
+  <>
     <BrowserView>
       <DesktopMenu/>
     </BrowserView>
     <MobileView>
       <MobileMenu/>
     </MobileView>
-  </div>
+  </>
 )
 
 export default Menu
