dd2d2799ddac4e2c8292b7a1c9ae63770e774ebb
William Suratt <williamsuratt@Williams-MacBook-Pro.local>
2021-03-13 21:27:36 -0500

    commit all changes

--- client/core/Menu.js
+++ client/core/Menu.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react'
 import { isMobile } from 'react-device-detect'
-import {BrowserView, MobileView} from "react-device-detect";
 import DesktopMenu from './DesktopMenu'
 import MobileMenu from './MobileMenu'
 
