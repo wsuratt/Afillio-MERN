9f4c6d0baeaa2963f276724be7e2297617368f53
William Suratt <williamsuratt@ip0af57840.int.uvm.edu>
2021-03-25 14:33:28 -0400

    commit all changes

--- client/list/AddToList.js
+++ client/list/AddToList.js
@@ -2,8 +2,8 @@ import React, {useState} from 'react'
 import {makeStyles} from '@material-ui/core/styles'
 import PropTypes from 'prop-types'
 import IconButton from '@material-ui/core/IconButton'
-import AddCartIcon from '@material-ui/icons/AddShoppingCart'
-import DisabledCartIcon from '@material-ui/icons/RemoveShoppingCart'
+import LibraryAddIcon from '@material-ui/icons/LibraryAdd';
+import HighlightOffIcon from '@material-ui/icons/HighlightOff';
 import list from './list-helper.js'
 import { Redirect } from 'react-router-dom'
 
@@ -34,10 +34,10 @@ export default function AddToList(props) {
     return (<span>
       {props.item.quantity >= 0 ?
         <IconButton color="secondary" dense="dense" onClick={addToList}>
-          <AddCartIcon className={props.listStyle || classes.iconButton}/>
+          <LibraryAddIcon className={props.listStyle || classes.iconButton}/>
         </IconButton> :
         <IconButton disabled={true} color="secondary" dense="dense">
-          <DisabledCartIcon className={props.listStyle || classes.disabledIconButton}/>
+          <HighlightOffIcon className={props.listStyle || classes.disabledIconButton}/>
         </IconButton>}
       </span>)
 }
